7edb20b0bf59c3fc6c0fe8829386d165f375b000
johnpgeipel <johnpgeipel@gmail.com>
2024-07-15 14:19:24 -0500

    first commit

---
+++ components/button.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+    count: number;
+    onClick: () => void;
+}
+
+export default function Button({ count, onClick }: ButtonProps) {
+    return (
+        <button
+            onClick={onClick}
+            className="bg-blue-500 hover:bg-blue-700 rounded text-white font-bold px-4 py-2">
+            I have been clicked {count} times
+        </button>
+    );
+}
